c140bb8145990e56f2093aaaa0a4fe17dd9549ed
Ravi Gadad <ravi@ablschools.com>
2022-01-18 01:49:27 -0800

    WIP

---
+++ lib/swarm/storage/hash/persistence.rb
@@ -0,0 +1,28 @@
+require_relative "../key_value/persistence"
+
+module Swarm
+  module Storage
+    module Hash
+      class Persistence < KeyValue::Persistence
+      def ids_for_type(type)
+        keys = store.keys.select { |key| key.match(regex_for_type(type)) }
+        keys.map { |key| key.gsub(regex_for_type(type), '\1') }
+      end
+
+      def all_of_type(type, subtypes: true)
+        store.select { |key, value|
+          key.match(regex_for_type(type)) &&
+            (subtypes || value["type"] == type)
+        }.values
+      end
+
+      def delete(key)
+        store.delete(key)
+      end
+
+      def truncate
+        store.clear
+      end
+    end
+  end
+end
---
+++ lib/swarm/storage/key_value/persistence.rb
@@ -0,0 +1,76 @@
+module Swarm
+  module Storage
+    module KeyValue
+      class Persistence
+        class AssociationKeyMissingError < StandardError; end
+
+        attr_reader :store
+
+        def initialize(store)
+          @store = store
+        end
+
+        def regex_for_type(type)
+          /^#{type}\:(.*)/
+        end
+
+        def ids_for_type(type)
+          raise "Not implemented yet!"
+        end
+
+        def all_of_type(type)
+          raise "Not implemented yet!"
+        end
+
+        def add_association(association_name, associated, owner:, class_name:, foreign_key: nil)
+          key = :"#{association_name}_ids"
+          if owner.respond_to?(key)
+            ids = owner.send(key) || owner.send("#{key}=", [])
+            ids << associated.id
+            associated
+          else
+            raise AssociationKeyMissingError, key
+          end
+        end
+
+        def load_associations(association_name, owner:, class_name:, foreign_key: nil)
+          key = :"#{association_name}_ids"
+          if owner.respond_to?(key)
+            ids = owner.send(key) || []
+            ids.map { |id|
+              self["#{class_name.split("::").last}:#{id}"]
+            }.compact
+          else
+            raise AssociationKeyMissingError, key
+          end
+        end
+
+        def serialize(value)
+          return nil if value.nil?
+          value.to_json
+        end
+
+        def deserialize(value)
+          return nil if value.nil?
+          JSON.parse(value)
+        end
+
+        def [](key)
+          deserialize(store[key])
+        end
+
+        def []=(key, value)
+          store[key] = serialize(value)
+        end
+
+        def delete(key)
+          raise "Not implemented yet!"
+        end
+
+        def truncate
+          raise "Not implemented yet!"
+        end
+      end
+    end
+  end
+end
